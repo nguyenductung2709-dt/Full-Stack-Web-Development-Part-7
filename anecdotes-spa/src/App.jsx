8cff737377cbc695c5171cd2dbe869dbf265e193
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-04 18:07:35 +0200

    7.4

--- anecdotes-spa/src/App.jsx
+++ anecdotes-spa/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useMatch, Navigate } from 'react-router-dom';
+import  { useField } from './hooks/index'
+
 
 const Menu = () => {
   const padding = {
@@ -61,18 +63,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const [redirect, setRedirect] = useState(false)
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     setRedirect(true)
@@ -88,15 +90,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input  {...content} /> 
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  {...author} /> 
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input  {...info} /> 
         </div>
         <button>create</button>
       </form>
