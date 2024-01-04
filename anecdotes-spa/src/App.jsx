a481f6a39bfabdb607ecebfa475719c8316f6c6c
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-04 15:50:44 +0200

    7.1

--- anecdotes-spa/src/App.jsx
+++ anecdotes-spa/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 const Menu = () => {
   const padding = {
@@ -6,9 +8,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <a href='/' style={padding}>anecdotes</a>
+      <a href='/create' style={padding}>create new</a>
+      <a href='/about' style={padding}>about</a>
     </div>
   )
 }
@@ -123,14 +125,21 @@ const App = () => {
   }
 
   return (
+    <>
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
       <Footer />
     </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<AnecdoteList anecdotes = {anecdotes}/>} />
+      <Route path="/create" element={<CreateNew addNew={addNew} />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+  <Footer/>
+    </>
   )
 }
 
